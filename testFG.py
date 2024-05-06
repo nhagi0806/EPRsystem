c7cd39244dd1b2ec210b1b8f59dfeaa70699016c
n.hagi0806 <iwgpoki6755@gmail.com>
2024-05-06 19:32:48 +0900

    a

--- testFG.py
+++ testFG.py
@@ -9,8 +9,20 @@ FG=rm.open_resource("USB0::0x0D4A::0x000D::9217876::INSTR")#Noda-san's FG
 # 関数ジェネレータの初期化
 FG = None
 
+def InitialSetFG():
+  FG.write(":SOURce:MODE BURSt") # 発信モードをバーストに設定
+  FG.write(":SOURce:FUNCtion:SHAPe USER") # 関数を任意波形(user)に設定
+  FG.write(":SOURce:BURSt:STATe 1") # 発信モードの連続/バースト切り替え(0/1 or OFF/ON)
+  FG.write(":SOURce:BURSt:MODE TRIGger") # バーストモードのタイプをトリガーに変更
+  FG.write(":SOURce:BURSt:NCYCles 1") # マーク波数を設定
+  FG.write(":SOURce:FREQuency %f" % (1000)) #周波数の設定(バーストの信号の長さの設定)
+  FG.write(":TRIGger:BURSt:SOURce EXTernal") # 外部トリガー設定
+  FG.write(":TRIGger:BURSt:SLOPe OFF") # 外部トリガーの極性をOFFにしておくと、外来ノイズによる誤動作が避けられる(マニュアルP.106)
+  FG.write(":SOURce:FUNCtion:USER %i" % 40000)  # メモリから任意波形読み込み
+  FG.write(":SOURce:VOLTage %f" % 2000) # 波形の振幅を設定
+  FG.write(":OUTPut:SYNC:BURSt:TYPE BSYNc") # SYNC OUTの同期出力をバースト同期に設定
 
-
+'''
 def InitialSetFG():
     # 関数ジェネレータの初期設定
     FG.write(f":SOURce:FREQuency {7500000}")  # 周波数を設定
@@ -24,6 +36,7 @@ def InitialSetFG():
     # FG.write(":OUTPut OFF")  # 出力をOFFにする
 
 # もし、他の設定を追加したい場合は、上記のように FG.write() を使って追加します
+'''
 
 # InitialSetFG()を呼び出して関数ジェネレータの初期設定を行う
 InitialSetFG()
